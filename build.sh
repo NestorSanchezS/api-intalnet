7fa7e5d33e0e06f10ccd2ed67570e4a3d11a74ca
NestorSanchez <nesaenz.69@gmail.com>
2023-01-28 00:32:49 -0500

    add file bilf.sh

---
+++ build.sh
@@ -0,0 +1,11 @@
+
+#!/bin/bash
+version=$1
+[ -z $version ] && version=0.1 && echo "\nset defaul version 0.1\n"
+
+docker build --no-cache -t nestorsanchezz/intalnet_api:$version .
+
+docker tag nestorsanchezz/intalnet_api:$version nestorsanchezz/intalnet_api:latest
+
+docker push nestorsanchezz/intalnet_api:$version
+docker push nestorsanchezz/intalnet_api:latest
